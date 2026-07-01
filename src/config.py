239590738bba06f98db90ebf00c7e6619647dd6c
ameeqio <ameeqakhtar@gmail.com>
2026-07-01 14:01:57 +0530

    feature engineering module implemented

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-
+# missing value imputations
 median_impute_missing: list[str] = ['Lot Frontage']
 
 zero_impute_missing: list[str] = ['Garage Cars', 'Garage Area', 'Mas Vnr Area', 'BsmtFin SF 1', 'BsmtFin SF 2', 
@@ -10,4 +10,21 @@ none_impute_missing: list[str] = ['Alley', 'Mas Vnr Type', 'Bsmt Exposure', 'Bsm
 
 mode_impute_missing: list[str] = ['Electrical']
 
-# later_handle = ['Garage Yr Blt']
+# engineering features
+age_features: dict[str, list[str]] = {
+    "HouseAge" : ["Yr Sold", "Year Built"],
+    "GarageAge" : ["Yr Sold", "Garage Yr Blt"],
+    "RemodelAge" : ["Yr Sold", "Year Remod/Add"]
+}
+
+area_features: dict[str, list[str]] = {
+    "TotalHousingSF" : ["Pool Area", "Garage Area", "Gr Liv Area", "Total Bsmt SF"],
+    "TotalPorchSF" : ["Wood Deck SF", "Open Porch SF", "Enclosed Porch", "3Ssn Porch", "Screen Porch"]
+}
+
+number_features: dict[str, list[str]] = {
+    "TotalBathrooms" : ['Bsmt Full Bath', 'Bsmt Half Bath', 'Full Bath', 'Half Bath']
+}
+
+drop_features: list[str] = ["Order", "PID"]
+# 'gargageAge' and 'garage yr' : 159 missing values
---
+++ src/feature_engineering.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+import pandas as pd
+from src.config import age_features, area_features, number_features, drop_features
+
+class FeatureEngineer:
+    def __init__(self, df: pd.DataFrame) -> None:
+        self.df: pd.DataFrame = df.copy()
+
+    def drop_ftrs(self) -> FeatureEngineer:
+        for ftr in drop_features:
+            self.df.drop(ftr, axis = 1, inplace = True)
+
+        return self
+
+    def fix_garage_yr(self) -> FeatureEngineer:
+        self.df.loc[self.df["Garage Yr Blt"] > self.df["Yr Sold"], "Garage Yr Blt"] = pd.NA
+        return self
+
+    def eng_area_ftrs(self) -> FeatureEngineer: 
+        for new_ftr, ftrs in area_features.items():
+            self.df[new_ftr] = self.df[ftrs].sum(axis = 1)
+
+        return self 
+        
+    def eng_number_ftrs(self) -> FeatureEngineer:
+        for new_ftr, ftrs in number_features.items():
+            self.df[new_ftr] = self.df[ftrs].sum(axis = 1)
+
+        return self
+    
+    def eng_age_ftrs(self) -> FeatureEngineer:
+        for new_ftr, (ftr1, ftr2) in age_features.items():
+            self.df[new_ftr] = self.df[ftr1] - self.df[ftr2]
+
+        return self
+    
+    def transform(self) -> pd.DataFrame:
+        return (
+            self.drop_ftrs()
+                .fix_garage_yr()
+                .eng_area_ftrs()
+                .eng_number_ftrs()
+                .eng_age_ftrs()
+                .df
+        )
